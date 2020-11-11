b167920e7e17739b0c5b71094ceca046ec43b553
srifalmi <73156111+srifalmi@users.noreply.github.com>
2020-11-11 23:06:36 +0700

    Memberikan tugas komponen paper

--- src/App.js
+++ src/App.js
@@ -2,43 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./COMPONENTS/Paper";
+import Header from "./COMPONENTS/Header";
+import TodoForm from "./COMPONENTS/TodoForm";
+import Todos from "./COMPONENTS/Todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-button main-black-color">Add</button>
-        <h1 className="header-title">TODO LISTS</h1>
-        <button className="header-button main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-button main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }
