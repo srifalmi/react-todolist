a0a26df74d623c77c7a4cf152db8921bdea04c24
srifalmi <73156111+srifalmi@users.noreply.github.com>
2020-12-16 21:57:38 +0700

    complete Todo Function (coret Todo)

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,28 +7,34 @@ import Todos from "../COMPONENTS/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "AKU SIAPA?" },
-    { text: "nama saya falmi" },
-    { text: "kamu tinggal dimana?" }
+    { text: "AKU SIAPA?", isCompleted: false },
+    { text: "nama saya falmi", isCompleted: false },
+    { text: "kamu tinggal dimana?", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
 
     setTodos(addedTodo);
   };
 
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
+
+    setTodos(addedTodo);
+  };
   const showAddToggle = () => setShowAdd(!showAdd);
 
-  console.log("showAdd", showAdd);
+  console.log("todos", todos);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
