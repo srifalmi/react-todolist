262ae1a8674472eeebbf092a9402fb5fd33b884e
srifalmi <73156111+srifalmi@users.noreply.github.com>
2020-12-19 23:48:05 +0700

    membuat button component (pengkompleksan button)

--- src/COMPONENTS/Header.jsx
+++ src/COMPONENTS/Header.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Button from "./Button";
+
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
     <section className="header">
-      <button
-        className="header-button main-black-color"
-        onClick={showAddToggle}
-      >
-        {showAdd ? "Finish" : "Add"}
-      </button>
-
+      <Button text={showAdd ? "Finish" : "Add"} onClick={showAddToggle} />
       <h1 className="header-title">TODO LISTS</h1>
-      <button className="header-button main-red-color"onClick={clearTodos}>Clear</button>
+      <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
 };
@@ -20,6 +16,6 @@ const Header = ({ showAddToggle, showAdd, clearTodos }) => {
 Header.propTypes = {
   showAddToggle: PropTypes.func.isRequired,
   showAdd: PropTypes.bool.isRequired,
-  clearTodos : PropTypes.func.isRequired
+  clearTodos: PropTypes.func.isRequired
 };
 export default Header;
